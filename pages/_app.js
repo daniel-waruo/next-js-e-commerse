521101109470ded95b178cd602ee389786111e8b
Daniel Waruo <warudaniel@gmail.com>
2020-03-03 09:45:12 +0300

    removed css import lines _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
 // IMPORT MDBOOTSTRAP CSS
 
 import '../css/dist/all.min.css';
-/*
-import '../css/dist/bootstrap.min.css';
-import '../css/dist/mdb.css';
-import '../assets/css/index.css'
-import '../assets/css/mdb-pro.css'
-*/
 import '../css/dist/style.css'
 
 // This default export is required in a new `pages/_app.js` file.
