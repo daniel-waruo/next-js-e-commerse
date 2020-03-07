7da517e1471de7403da01ea3e0d216fea16eec89
Daniel Waruo <warudaniel@gmail.com>
2020-03-07 10:23:50 +0300

    api url not changed during shift to production

--- _constants.jsx
+++ _constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL = 'http://127.0.0.1:8000';
+export const API_URL = 'http://develop-api.herokuapp.com';
 
 export const GRAPHQL_ENDPOINT = `${API_URL}/graph-ql`;
 /******************************
