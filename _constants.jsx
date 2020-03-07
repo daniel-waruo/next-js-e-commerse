479c886385100e2a1565a24555a96433bfe9e64c
Daniel Waruo <warudaniel@gmail.com>
2020-03-07 07:39:49 +0300

    another api url fix

--- _constants.jsx
+++ _constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL = 'http://develop-api.herokuapp.com';
+export const API_URL = 'https://develop-api.herokuapp.com';
 
 export const GRAPHQL_ENDPOINT = `${API_URL}/graph-ql`;
 /******************************
