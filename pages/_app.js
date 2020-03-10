ea7817ceb999892ff4b2f9cf33223db9f1860376
Daniel Waruo <warudaniel@gmail.com>
2020-03-10 20:36:08 +0300

    added title , favicon and google search console authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 // IMPORT MDBOOTSTRAP CSS
 import '../css/dist/all.min.css';
-import '../assets/css/mdb-pro.css'
-import '../css/dist/style.css'
+import '../assets/css/mdb-pro.css';
+import '../css/dist/style.css';
+import React from 'react';
+import Head from 'next/head';
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <title>Next E-commerce</title>
+        <link rel="shortcut icon" href={"/favicon.ico"} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
