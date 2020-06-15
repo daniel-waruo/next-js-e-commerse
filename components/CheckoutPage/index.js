5d2abe411ee3da469e09777ec14f7b101ede0e8d
Daniel Waruo <waruodaniel@gmail.com>
2020-06-15 14:32:29 +0300

    added stepper on checkout page

--- components/CheckoutPage/index.js
+++ components/CheckoutPage/index.js
@@ -5,9 +5,11 @@ import {graphql} from 'react-apollo';
 import compose from "lodash.flowright";
 import {cartQuery} from "../CartPage/queries";
 import MainLoader from "../MainLoader";
+import {StepperSection} from "./components";
 
 class CheckoutPage extends React.PureComponent {
   state = {}
+
   render() {
     const {data: {loading, error, cart}} = this.props
     if (loading) return <MainLoader/>;
@@ -15,7 +17,9 @@ class CheckoutPage extends React.PureComponent {
     return (
       <MDBRow>
         <MDBCol size={"12"} md={"9"}>
-          <h1 className={"text-center "}>Checkout Page</h1>
+          <div className={"mt-5 mx-2"}>
+            <StepperSection initialID={'him'}/>
+          </div>
         </MDBCol>
         <MDBCol size={"12"} md={"3"}>
           <OrderSummary cart={cart}
